257de6dc4d9ff229846d3698c8ea238067a1561f
Santhosh Kumar <santhos995@gmail.com>
2022-01-28 14:40:09 +0530

    Unique path - iterative

---
+++ Problems/UniquePath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class UniquePath
+    {
+        public int UniquePaths(int m, int n)
+        {
+            int[] dp = new int[n];
+            Array.Fill(dp, 1);
+            int prev = 1;//it is also base case
+            for (int i = 1; i < m; i++)
+            {
+                prev = 1;//reset here for every row
+                for (int j = 1; j < n; j++)
+                {
+                    int ways  = prev + dp[j];
+                    dp[j] = ways;
+                    prev = ways;
+                }
+            }
+            return dp[n - 1];
+        }
+    }
+}
