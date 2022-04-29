263fd296010e714b32038309ce03fba8f66c6e7d
Santhosh Kumar <santhos995@gmail.com>
2022-04-29 13:38:09 +0530

    BiPartite graph DFS implementation

--- bipartiteGraph.js
+++ bipartiteGraph.js
@@ -1,4 +1,4 @@
-var isBipartite = function (graph) {
+var isBipartiteBFS = function (graph) {
     var n = graph.length;
     var color = new Array(n);
     var q = [];
@@ -23,7 +23,33 @@ var isBipartite = function (graph) {
     return true;
 }
 
+var isBipartiteDFS = function (graph) {
+    var n = graph.length;
+    let color = new Array(n).fill(0);
+    for (let i = 0; i < graph.length; i++) {
+        
+        if(color[i]==0 && !validColor(graph, color,1,i)){
+            return false;
+        }
+        
+    }
+    return true;
+}
+var validColor = function(graph, colors, color, idx){
+    if(colors[idx]!=0)
+        return (colors[idx]==color);
+    
+    colors[idx] = color;
+    for(var i=0;i<graph[idx].length;i++){
+        if(!validColor(graph, colors,-color,graph[idx][i]))
+            return false;
+    }
+    return true;
+}
 
-console.log(isBipartite([[], [3], [], [1], []]));
-console.log(isBipartite([[1, 2, 3], [0, 2], [0, 1, 3], [0, 2]]));
-console.log(isBipartite([[1, 3], [0, 2], [1, 3], [0, 2]]));
+console.log(isBipartiteBFS([[], [3], [], [1], []]));
+console.log(isBipartiteBFS([[1, 2, 3], [0, 2], [0, 1, 3], [0, 2]]));
+console.log(isBipartiteBFS([[1, 3], [0, 2], [1, 3], [0, 2]]));
+console.log(isBipartiteDFS([[], [3], [], [1], []]));
+console.log(isBipartiteDFS([[1, 2, 3], [0, 2], [0, 1, 3], [0, 2]]));
+console.log(isBipartiteDFS([[1, 3], [0, 2], [1, 3], [0, 2]]));
